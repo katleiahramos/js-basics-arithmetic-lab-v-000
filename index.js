72ea2f4dad2e99b11f80430311935fa7e131bbbd
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 21:58:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ const randomNumber = ((Math.random() * 10) + 10)  ;
 
 const randomInteger = Math.floor(randomNumber);
 
-debugger; 
+console.log(newID + randomIntege)
 const randomUserID = newID + randomInteger;
